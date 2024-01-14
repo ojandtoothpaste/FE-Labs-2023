9f0ff7a6cadd7b86ddb9e9181539046773bc042c
Gabe Swenson <gswen75@icloud.com>
2024-01-13 18:08:32 -0700

    Week 7 labs

--- Lab07/JS4Lab.js
+++ Lab07/JS4Lab.js
@@ -43,9 +43,14 @@ Question 1: Declaring Let and Const variables`)
  * Step 2: Initialize two more variables using 'let' and set one value to your name, and the other to your friends name.
  *
  * ↓ YOUR CODE HERE ↓ */
+const food = 'Pot Pie';
+const drink = 'Coffee';
+let myName = 'Gabe';
+let friendsName = 'Leo';
 
+console.log(food, drink, myName, friendsName);
 //Question: Name a reason why you would use const instead of let.
-//Answer:
+//Answer:const can't be changed
 
 /*---------------------------------- Template Literals -------------------------------------*/
 console.log(`-------------------------- 
@@ -57,10 +62,11 @@ Question 2: Template Literals`)
  *
  *
  * ↓ YOUR CODE HERE ↓ */
-
+console.log(
+`My friend ${friendsName} and ${myName} are eating ${food} and drinking ${drink}`)
 //Question: How would the sentence you wrote look if you used string concatenation instead of template literals?
 //          console.log() your answer to check
-//Answer:
+//Answer: 'My friend '+ friendsName + 'And '+ myName  + 'are eating ' + food + 'and drinking ' + drink 
 console.log(`-------------------------- 
 Question 2a: Oh no`)
 
@@ -70,9 +76,10 @@ Question 2a: Oh no`)
  * Step 2: Make a change to your code in Question 1 to fix the error. Run your code to make sure it's fixed.
  *
  * ↓ YOUR CODE HERE ↓ */
-
+let drink = 'Apple Juice';
+console.log(drink);
 //Question: Read the error message - in your own words - what does it mean?
-//Answer:
+//Answer: A const variable can't be changed
 
 /*---------------------------------- Arrow Functions -------------------------------------*/
 console.log(`-------------------------- 
@@ -90,9 +97,14 @@ Question 3: Arrow Functions`)
  * Step 3: Copy the code below, ↓ paste it underneath our new function, and run the code.
  *         console.log(lunchTime('Pizza','Water'))
  * ↓ YOUR CODE HERE ↓ */
-
+const lunchTime = (foodChoice, drinkChoice) => {
+   return `My friend ${friendsName} and ${myName} are eating ${foodChoice} and drinking ${drinkChoice}`
+}
+console.log(lunchTime('Pizza','Water'));
 //Question: What would your function look like if we were using the old way of creating a function?
-//Answer:
+//Answer: function lunchTime(foodChoice, drinkChoice) {
+ //  return `My friend ${myFriendsName} and ${myName} are eating ${foodChoice} and drinking ${drinkChoice}`
+ // }
 
 /*---------------------------------- Callbacks -------------------------------------*/
 console.log(`-------------------------- 
@@ -107,5 +119,8 @@ Question 4: Callbacks`)
  * Step 3: console.log() your new function, foodForTodayAndTomorrow, and pass in lunchTime('newFoodItem', 'newDrinkItem') as the argument for the "callbackFunction" parameter.
  *
  * ↓ YOUR CODE HERE ↓ */
-
+const foodForTodayAndTomorrow = (tomorrowsFoodChoice, tomorrowsDrinkChoice, callBackFunction) => {
+   return `${callbackFunction} Tomorrow we want ${tomorrowsFoodChoice} and ${tomorrowsDrinkChoice}.`
+}
+HTMLFormControlsCollection.log(foodForTodayAndTomorrow('Pizza', 'Soda', lunchTime('Cheese', 'AppleJuice')));
 console.log(`-----------Finished------------`)
